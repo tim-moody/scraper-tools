f6818613e1c323b7805e191059c7aff0d0ffe6f5
Tim Moody <tim@timmoody.com>
2020-12-17 11:24:16 -0500

    make-zim.sh

---
+++ sites/nih-rare-diseases/make-zim.sh
@@ -0,0 +1,5 @@
+zimwriterfs --welcome=diseases/browse-by-first-letter/A/index.html --favicon=assets/GARD_logo.png --language="eng" --title="NIH Genetic and Rare Diseases" \
+ --description="NIH GARD Compendium of more than 6000 articles on Genetic and Rare Diseases. Includes a glossary and Spanish translations." \
+ --creator="GARD" --publisher="Internet in a Box" \
+ --tags="Medical Reference;_category:medical;_pictures:yes;_videos:no;_details:yes" \
+ /library/www/html/modules/en-nih_rarediseases /Files/Scraped/nih_rarediseases_en_all_maxi_2020-12.zim
