cf221547f4adfebd5dff3dc8defdd59c49080fe2
Tim Moody <tim@timmoody.com>
2021-01-19 08:34:10 -0500

    moved earlier version

---
+++ sites/gcfglobal/FromZips/make-zim.sh
@@ -0,0 +1,5 @@
+zimwriterfs --welcome=index.html --favicon=assets/logo.png --language="eng" --title="GCF2020 Learn Free" \
+ --description="Hundreds of high-quality illustrated articles and videos focusing on technology, job training, reading, and math skills, produced by the Goodwill Community Foundation." \
+ --creator="GCF" --publisher="Internet in a Box" \
+ --tags="Training;_category:educational;_pictures:yes;_videos:yes;_details:yes" \
+ /library/www/html/modules/en-GCF2020 /Files/Scraped/gcf2020_en_all_maxi_2020-12.zim
